340f0cfe5b78346ea51d55e5c32d7e57195aba10
SergioPint <sergio@pintadu.ch>
2023-10-15 09:04:56 +0200

    final code

---
+++ graphql-server/index.js
@@ -0,0 +1,87 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+// db
+import _db from './_db.js';
+
+// types
+import { typeDefs } from './schema.js';
+
+const resolvers = {
+  Query: {
+    games() {
+      return _db.games;
+    },
+    game(_, args) {
+      return _db.games.find((game) => game.id === args.id);
+    },
+    reviews() {
+      return _db.reviews;
+    },
+    review(_, args) {
+      return _db.reviews.find((review) => review.id === args.id);
+    },
+    authors() {
+      return _db.authors;
+    },
+    author(_, args) {
+      return _db.authors.find((author) => author.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return _db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return _db.reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return _db.authors.filter((author) => author.id === parent.author_id);
+    },
+    game(parent) {
+      return _db.games.filter((game) => game.id === parent.game_id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      _db.games = _db.games.filter((game) => game.id !== args.id);
+      return _db.games;
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000),
+      };
+      _db.games.push(game);
+      return game;
+    },
+    updateGame(_, args) {
+      _db.games = _db.games.map((game) => {
+        if (game.id === args.id) {
+          return { ...game, ...args.edits };
+        }
+        return game;
+      });
+
+      return _db.games.find((game) => game.id === args.id);
+    },
+  },
+};
+
+// server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const port = 4000;
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: port },
+});
+
+console.log(`Server ready at port ${port}`);
